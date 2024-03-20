1afe906dc5769bda5efddfb630f794550e976fbb
Mohammed Taqi <taqealibade@gmail.com>
2024-03-20 22:10:57 +0300

    File based routing

---
+++ pages/course.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class=" p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center">
+        <div class=" prose mb-12">
+            <h1>
+                <span class=" font-medium">Course:</span>
+                <span class=" font-bold">Mastering Nuxt</span>
+            </h1>
+        </div>
+
+    <div class="flex flex-row justify-center flex-grow">
+        <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-row">
+            <h3>Chapters</h3>
+        </div>
+            <div class="prose p-12 bg-white rounded-md w-[65ch]">
+                <h2>Lesson</h2>
+                <p>This is a Lesson</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
